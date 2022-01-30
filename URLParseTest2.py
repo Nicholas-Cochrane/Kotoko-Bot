25d242e433876c82c9338f01d0b3966eb087bdec
Greenwolf25 <nicholascochrane25@gmail.com>
2022-01-29 21:02:03 -0800

    Added wikitextClean and gtts
    Added falling space for tags removed by HTMLParser
    Added wikitextClean(str)
    can clean up links[[A|B]], headers=, bolding''', And __TOC__. Can also fix URL safe ampersands, and removed empty space
    
    imported google text to speech

--- URLParseTest2.py
+++ URLParseTest2.py
@@ -1,7 +1,10 @@
 import urllib.request
 import urllib
+import re
 import ssl
 import json
+import gtts
+from playsound import playsound
 
 from io import StringIO
 from html.parser import HTMLParser
@@ -15,6 +18,8 @@ class MLStripper(HTMLParser):
         self.text = StringIO()
     def handle_data(self, d):
         self.text.write(d)
+    def handle_endtag(self, tag):
+        self.text.write(' ')
     def get_data(self):
         return self.text.getvalue()
 
@@ -23,6 +28,22 @@ def strip_tags(html):
     s.feed(html)
     return s.get_data()
 
+def wikitextClean(str):
+    '''clean up wikitext remove [[]], =, and \'\'\' '''
+    markedStr = str.replace('[[','[[@inside@').replace('%26', '&') #add marker for inside of link and clean up &'s from link safe to normal
+    temp = re.split('\[\[|\]\]',markedStr) #split on links [[X]] -> X
+    for index, substring in enumerate(temp):
+        x = substring.find('|')
+        if (x != -1):
+            temp[index] = substring[x+1:] #replace [[A|B]] -> B
+            
+        if(temp[index].find(':')!= -1 and temp[index].find('@inside@')!= -1):
+            temp[index] = '' #remove [[X]]'s that contain a : EX: [[Catagory:verb]]->Verb
+        #todo ignore <code> tags (modifiy html stripper or pre htlm stripper)
+        #todo remove links and clean up or remove tables
+    newStr = ''.join(temp).replace('@inside@','').replace('=','').replace('\'\'\'','')#remove fromating like == and ''' 
+    return re.sub('( \s*)\n','\n',re.sub('__.*?__','',newStr)) #remove__X__ used for things like table of contents and can be in templates and remove empty lines
+     
 #https://en.wikipedia.org/w/api.php?action=help&modules=parse
 #https://www.mediawiki.org/wiki/API:Query
 ssl._create_default_https_context = ssl._create_unverified_context #SSL certificate for https
@@ -31,7 +52,7 @@ with urllib.request.urlopen('https://en.wikipedia.org/w/api.php?action=query&for
     for key, value in pageObj['query']['pages'].items():
         print("\n")
         print(pageObj['query']['pages'][key]['title'])
-        #print(pageObj['query']['pages'][key]['extract'])
+        print(pageObj['query']['pages'][key]['extract'])
 
 #with user agent defined as mozilla to avoid simple bot detection
 req = urllib.request.Request('https://wiki.ss13.co/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Cluwne')
@@ -42,7 +63,7 @@ with urllib.request.urlopen(req) as r:
     for key, value in pageObj2['query']['pages'].items():
         print("\n")
         print(pageObj2['query']['pages'][key]['title'])
-        print(pageObj2['query']['pages'][key]['revisions'])
+        print(wikitextClean(pageObj2['query']['pages'][key]['revisions'][0]['*']))
 
 req2 = urllib.request.Request("https://wiki.warthunder.com/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Wyvern_S4")
 # Customize the default User-Agent header value:
@@ -50,21 +71,22 @@ req2.add_header('User-Agent', 'Mozilla/5.0')
 with urllib.request.urlopen(req2) as r2:
     pageObj3 = json.loads(r2.read())
     for key, value in pageObj3['query']['pages'].items():
-        print("\n")
-        print(pageObj3['query']['pages'][key]['title'])
+        #print("\n")
+        #print(pageObj3['query']['pages'][key]['title'])
         wikiText = pageObj3['query']['pages'][key]['revisions'][0]['*']
         wikiText = wikiText.replace('&','%26')
-        #todo replace [[A|B]] -> B and [[X]] -> X
-        print(wikiText)
-        print('\n')
+        #print(wikiText)
+        """print('\n')
         
         print("https://wiki.warthunder.com/api.php?action=expandtemplates&format=json&prop=wikitext&title=Wyvern_S4&text={}".format(urllib.parse.quote_plus(wikiText)[:3000]))
-        print('\n')
-        req3 = urllib.request.Request("https://wiki.warthunder.com/api.php?action=expandtemplates&format=json&prop=wikitext&title=Wyvern_S4&text={}".format(urllib.parse.quote_plus(wikiText)[:3000]))
+        print('\n')"""
+        req3 = urllib.request.Request("https://wiki.warthunder.com/api.php?action=expandtemplates&format=json&prop=wikitext&title=Wyvern_S4&text={}".format(urllib.parse.quote_plus(wikiText)[:5000]))
         req3.add_header('User-Agent', 'Mozilla/5.0')
         with urllib.request.urlopen(req3) as r3:
             pageObj4 = json.loads(r3.read())
             for key, value in pageObj4.items():
-                print(strip_tags(pageObj4['expandtemplates']['wikitext']))
-                #todo Removed all [[]]
+               #print(strip_tags(pageObj4['expandtemplates']['wikitext']))
+               print(wikitextClean(strip_tags(pageObj4['expandtemplates']['wikitext'])))
     
+#tts = gtts.gTTS(wikitextClean(strip_tags(pageObj4['expandtemplates']['wikitext'])))
+#tts.save("test.mp3")
